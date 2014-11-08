dc1dd71d57388f59596effafcf3faec9fd7d64cb
keirp <keiran@me.com>
2014-11-08 13:22:30 -0800

    FREAKING FINALLY I ADDED ALLL THE THINGS

--- Assets/Scripts/DaysOneThroughSeven.cs
+++ Assets/Scripts/DaysOneThroughSeven.cs
@@ -5,6 +5,7 @@ public class DaysOneThroughSeven : MonoBehaviour {
 
 	public Transform hurdle;
 	public Transform marker;
+	public Transform abyss;
 	private float lastObst = 0.0f;
 	private float obstSpawnInt = 2.0f;
 	private float lastMarker = 0.0f;
@@ -19,7 +20,16 @@ public class DaysOneThroughSeven : MonoBehaviour {
 	void Update () {
 		if (Time.time - lastObst > obstSpawnInt) {
 			lastObst = Time.time;
-			Instantiate(hurdle, new Vector3(.52f, 1.21f, transform.position.z), Quaternion.identity);
+			int obst = Random.Range(0, 2);
+			switch (obst) {
+				case 0:
+					Instantiate(hurdle, new Vector3(.52f, 1.21f, transform.position.z), Quaternion.identity);
+					break;
+				case 1:
+					Instantiate(abyss, new Vector3(0f, 1.229f, transform.position.z), Quaternion.identity);
+					break;
+			}
+
 		}
 		if (Time.time - lastMarker > markerSpawnInt) {
 			lastMarker = Time.time;
